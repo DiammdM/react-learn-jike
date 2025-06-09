1241c283ee6d72c7101858fedd765de0b2a02118
diammd <1015906008@qq.com>
2025-06-09 21:00:47 +0800

    feat：add whiteList

--- src/components/AuthRoute.jsx
+++ src/components/AuthRoute.jsx
@@ -1,15 +1,20 @@
-import { getToken } from '@/utils'
-import React from 'react'
-import { Navigate } from 'react-router-dom'
+import { getToken } from '@/utils';
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+
+const ignorePaths = ['/login', '/register'];
 
 const AuthRoute = ({ children }) => {
-    if (getToken()) {
-        return (
-            <>{children}</>
-        )
-    } else {
-        return <Navigate to={'/login'} replace />
-    }
-}
+  // 如果是忽略的路径，直接返回 children
+  if (ignorePaths.includes(window.location.pathname)) {
+    return <>{children}</>;
+  }
 
-export default AuthRoute
+  if (getToken()) {
+    return <>{children}</>;
+  } else {
+    return <Navigate to={'/login'} replace />;
+  }
+};
+
+export default AuthRoute;
